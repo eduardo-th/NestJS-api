dc8d2f949ce8bdbd68de388fe27a6026e44dd560
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2023-06-20 16:42:34 -0300

    feat: add mongo database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>{
+        return {
+          uri: configService.get('DATABASE_URL'),
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
