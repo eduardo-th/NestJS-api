6f77270c3c82abb83be435be93f81581078892ee
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2023-06-21 16:01:08 -0300

    feat: add post crud resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
@@ -10,14 +11,15 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) =>{
+      useFactory: async (configService: ConfigService) => {
         return {
           uri: configService.get('DATABASE_URL'),
           useNewUrlParser: true,
           useUnifiedTopology: true,
-        }
-      }
-    })
+        };
+      },
+    }),
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
