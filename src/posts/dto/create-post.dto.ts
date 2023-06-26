32b966d1a9ff7ad3bf3a983cf1843bf91af4dcbe
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2023-06-26 16:33:13 -0300

    feat: add create post DTO

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,25 @@
-export class CreatePostDto {}
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  ArrayNotEmpty,
+  IsArray,
+  IsString,
+} from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  body: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @ArrayMinSize(1)
+  @ArrayMaxSize(10)
+  @IsString({ each: true })
+  tags: string[];
+
+  @IsString()
+  author: string;
+}
