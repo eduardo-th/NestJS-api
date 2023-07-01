21b3f0e13965056bef89ad0beb467f17f85a63c8
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2023-07-01 14:48:30 -0300

    style: remove white spaces add proper indentation

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  app.useGlobalPipes(new ValidationPipe({ 
-    whitelist: true, 
-    transform: true,
-    transformOptions:{
-      enableImplicitConversion: true
-    }
-   }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   await app.listen(configService.get('PORT'));
 }
