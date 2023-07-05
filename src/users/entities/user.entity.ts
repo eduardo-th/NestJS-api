41fbbd51e9283801f51927cdc96ebbf86fd40619
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2023-07-05 16:11:33 -0300

    feat: add user schema

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,3 +1,44 @@
-export class User{
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+
+@Schema({_id: false})
+class Avatar extends Document{
+
+    @Prop({default: 'https://res.cloudinary.com/dqfvb6su5/image/upload/v1666904254/social-media/avatars/mustache2_ptqjy3.jpg'})
+    url: string
+    
+    @Prop({default: ''})
+    filename: string
 
 }
+const avatarSchema = SchemaFactory.createForClass(Avatar)
+
+@Schema({
+    timestamps: true,
+    toJSON: {virtuals: true},
+    toObject: {virtuals: true}
+})
+export class User extends Document{
+
+    @Prop({required: true, unique: true})
+    username: string
+
+    @Prop({required: true})
+    password: string
+
+    @Prop({required: true, unique: true})
+    email: string
+
+    @Prop({default: 'somthing about me'})
+    about: string
+
+    @Prop({type: avatarSchema, default: {}})
+    avatar: Avatar
+
+    @Prop({type:[{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}]})
+    followers: User[]
+
+    @Prop({type:[{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}]})
+    follows: User[]
+}
+export const UserSchema = SchemaFactory.createForClass(User)
