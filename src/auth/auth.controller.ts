f347e4c6d3a820feef4480c4220e9eb01a8819d3
eduardo-th <105067740+eduardo-th@users.noreply.github.com>
2023-07-17 22:24:37 -0300

    refactor: move register route from user to auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,14 +5,21 @@ import {
 } from '@nestjs/common';
 import { LoginUserDto } from './dto/login-user.dto';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private usersService: UsersService) {}
 
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
     const { username, password } = loginUserDto;
     return this.authService.login(username, password);
+  }
+
+  @Post('register')
+  registerUser(@Body() createUserDto: CreateUserDto){
+    return this.usersService.create(createUserDto);
   }
 }
